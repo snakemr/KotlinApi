710db02b9ec0f669bf628d858aa07a41f74b3a1c
mad.lrmk.ru <admin@lrmk.ru>
2023-09-08 13:54:44 +0300

    HTTP Api & Client template

--- src/main/kotlin/my/example/plugins/Routing.kt
+++ src/main/kotlin/my/example/plugins/Routing.kt
@@ -3,7 +3,6 @@ package my.example.plugins
 import app.cash.sqldelight.driver.jdbc.sqlite.JdbcSqliteDriver
 import io.ktor.server.application.Application
 import io.ktor.server.application.call
-import io.ktor.server.html.respondHtml
 import io.ktor.server.request.receiveParameters
 import io.ktor.server.response.respond
 import io.ktor.server.response.respondText
@@ -29,13 +28,13 @@ fun Application.configureRouting() {
             }
         }
 
-        // При обращении к /users выдаёт полный список пользователей в виде JSON
+        // При обращении к /users выдаётся полный список пользователей в виде JSON
         get("users") {
             val users = database.usersQueries.allUsers().executeAsList()
             call.respond(users)
         }
 
-        // При отправке поля "name" на адрес /add добавляет пользователя в таблицу
+        // При отправке поля "name" на адрес /add пользователь добавляется в таблицу
         post("add") {
             val name = call.receiveParameters()["name"] ?: return@post
             database.usersQueries.insert(name)
