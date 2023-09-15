88c6778b065751c970aef90bd7f4154d680f1aa5
mad.lrmk.ru <admin@lrmk.ru>
2023-09-15 14:49:31 +0300

    HTTP Api & Client template

--- src/main/kotlin/my/example/plugins/Routing.kt
+++ src/main/kotlin/my/example/plugins/Routing.kt
@@ -3,13 +3,16 @@ package my.example.plugins
 import app.cash.sqldelight.driver.jdbc.sqlite.JdbcSqliteDriver
 import io.ktor.server.application.Application
 import io.ktor.server.application.call
+import io.ktor.server.request.receive
 import io.ktor.server.request.receiveParameters
 import io.ktor.server.response.respond
 import io.ktor.server.response.respondText
+import io.ktor.server.routing.delete
 import io.ktor.server.routing.get
 import io.ktor.server.routing.post
 import io.ktor.server.routing.routing
 import my.example.Database
+import my.example.User
 
 fun Application.configureRouting() {
     val driver = JdbcSqliteDriver("jdbc:sqlite:database.s3db")
@@ -20,8 +23,13 @@ fun Application.configureRouting() {
         // При первом обращении к корню / сервиса создаётся база данных
         get("/") {
             try {
-                database.usersQueries.allUsers().executeAsList()
-                call.respondText("API готов к работе")
+                database.userQueries.allUsers().executeAsList()
+                call.respondText("""API готов к работе.
+                    |GET users: Вывод всех пользователей (json)
+                    |POST add: Добавить пользователя (поле name)
+                    |POST new: Добавить пользователя (json)
+                    |DELETE user/№: Удалить пользователя №
+                """.trimMargin())
             } catch (_: Exception) {
                 Database.Schema.create(driver)
                 call.respondText("База данных успешно создана")
@@ -30,16 +38,36 @@ fun Application.configureRouting() {
 
         // При обращении к /users выдаётся полный список пользователей в виде JSON
         get("users") {
-            val users = database.usersQueries.allUsers().executeAsList()
+            val users = database.userQueries.allUsers().executeAsList()
             call.respond(users)
+        }
+
+        // При обращении к /user/№ выдаётся объект "пользователь" виде JSON
+        get("user/{id}") {
+            val id = call.parameters["id"]?.toLongOrNull() ?: return@get
+            val user = database.userQueries.user(id).executeAsOneOrNull()
+            if (user != null) call.respond(user)
         }
 
         // При отправке поля "name" на адрес /add пользователь добавляется в таблицу
         post("add") {
             val name = call.receiveParameters()["name"] ?: return@post
-            database.usersQueries.insert(name)
+            database.userQueries.insert(name)
             call.respondText("Пользователь добавлен")
         }
 
+        // При отправке json-объекта "User" на адрес /new пользователь добавляется в таблицу
+        post("new") {
+            val user = call.receive<User>()
+            database.userQueries.add(user)
+            call.respondText("Пользователь добавлен")
+        }
+
+        // При запросе удаления по адресу /user/№ пользователь удаляется из таблицы
+        delete("user/{id}") {
+            val id = call.parameters["id"]?.toLongOrNull() ?: return@delete
+            database.userQueries.delete(id)
+            call.respondText("Пользователь удалён")
+        }
     }
 }
