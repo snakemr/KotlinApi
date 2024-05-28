bd2bf7ced396a6d16743a6394970ca152cf94671
mad.lrmk.ru <admin@lrmk.ru>
2024-05-28 10:22:22 +0300

    update kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ val logback_version: String by project
 val sqlite_version: String by project
 
 plugins {
-    kotlin("jvm") version "1.9.21"
+    kotlin("jvm") version "1.9.23"
     id("io.ktor.plugin") version "2.3.7"
     id("app.cash.sqldelight") version "2.0.1"
 }
